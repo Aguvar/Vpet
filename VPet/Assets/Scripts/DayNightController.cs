02206b5ac7d16f84ae8a1e77fa7fdbd510679895
Augusto Varela Banchero <gustivarela@hotmail.com>
2017-11-16 15:51:25 +0100

    Finished fence and developed day/night cycle

---
+++ VPet/Assets/Scripts/DayNightController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightController : MonoBehaviour {
+
+    private float currentTimeInSeconds;
+    private Transform sunTransform;
+    private Light sun;
+
+    //Range[0,1]
+    private float timeOfTheDay;
+    private float sunIntensity;
+
+    private readonly float secondsInADay = 86400;
+
+    // Use this for initialization
+    void Start () {
+        sunTransform = GetComponent<Transform>();
+        currentTimeInSeconds = (float)System.DateTime.Now.TimeOfDay.TotalSeconds;
+        sun = GetComponent<Light>();
+        sunIntensity = sun.intensity;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentTimeInSeconds = (float)System.DateTime.Now.TimeOfDay.TotalSeconds;
+
+        timeOfTheDay = currentTimeInSeconds / secondsInADay;
+
+        UpdateSun();
+    }
+
+    private void UpdateSun()
+    {
+        sunTransform.localRotation = Quaternion.Euler((timeOfTheDay * 360f) - 90, 170, 0);
+
+        //float intensityMultiplier = 1;
+        //if (currentTimeOfDay <= 0.23f || currentTimeOfDay >= 0.75f)
+        //{
+        //    intensityMultiplier = 0;
+        //}
+        //else if (currentTimeOfDay <= 0.25f)
+        //{
+        //    intensityMultiplier = Mathf.Clamp01((currentTimeOfDay - 0.23f) * (1 / 0.02f));
+        //}
+        //else if (currentTimeOfDay >= 0.73f)
+        //{
+        //    intensityMultiplier = Mathf.Clamp01(1 - ((currentTimeOfDay - 0.73f) * (1 / 0.02f)));
+        //}
+
+        //sun.intensity = sunInitialIntensity * intensityMultiplier;
+    }
+}
