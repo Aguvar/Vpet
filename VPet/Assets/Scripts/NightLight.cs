a459081e426fac0476f406a4382c195294c7fd01
Augusto Varela Banchero <gustivarela@hotmail.com>
2017-11-30 15:30:03 +0100

    Started work on Nightlights

--- VPet/Assets/Scripts/NightLight.cs
+++ VPet/Assets/Scripts/NightLight.cs
@@ -1,14 +1,33 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
+[RequireComponent(typeof(Light))]
 public class NightLight : MonoBehaviour {
+
+    public float baseIntensity;
+    public float flickerRange;
+
+    private Light lightSource;
 
 	// Use this for initialization
 	void Start () {
-		
+        lightSource = GetComponent<Light>();
+        lightSource.intensity = baseIntensity;
+
+        StartCoroutine(Flicker());
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    private IEnumerator Flicker()
+    {
+        float intensityChange = Random.Range(-flickerRange, flickerRange);
+
+        lightSource.intensity = baseIntensity + intensityChange;
+
+        yield return new WaitForSeconds(0.2f);
+    }
 }
